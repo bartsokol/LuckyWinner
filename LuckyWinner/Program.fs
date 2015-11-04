d0762ce55824f4ca98cafe3b11558aa465472b3b
Bart Sokol <sokol.bartosz@gmail.com>
2015-11-04 17:54:59 +0100

    Initial version of LuckyWinner

---
+++ LuckyWinner/Program.fs
@@ -0,0 +1,48 @@
+﻿let fileNotFound = "Provided file does not exist."
+let incorrectNumber = "Provided number is invalid."
+
+let getRandomNotInList list generator =
+    let mutable result = generator ()
+    while List.contains result list do
+        result <- generator ()
+    result
+
+let rec uniqueRandom itemGenerator (current : list<'a>) number =
+    match current.Length with
+    | x when x < number -> uniqueRandom itemGenerator (List.concat [current; [(getRandomNotInList current itemGenerator)]]) number
+    | number -> current
+
+let getRandomRowNumbers lineCount amount =
+    let r = System.Random()
+    uniqueRandom (fun () -> r.Next(0, lineCount)) [] amount
+
+let displayWinners items =
+    Seq.iter (printfn "* %s") items
+
+let proceed filename number =
+    let lines = System.IO.File.ReadAllLines(filename)
+    printfn "Selecting %A lucky winners out of %A" number lines.Length
+    match lines.Length with
+    | 0 -> printfn "File is empty."
+    | count when count <= number -> printfn "Not enough data. Expected more than %A lines, got %A lines." number count
+    | count -> (getRandomRowNumbers count number) |> Seq.map (fun ind -> lines.[ind]) |> displayWinners
+    ()
+
+let parse number =
+    match System.Int32.TryParse(number) with
+    | (true,int) -> Some(int)
+    | _ -> None
+
+let verify filename number =
+    match (System.IO.File.Exists(filename), number) with
+    | (true, Some num) -> proceed filename num
+    | (true, None) -> incorrectNumber |> (printfn "%s")
+    | (false, Some _) -> fileNotFound |> (printfn "%s")
+    | (false, None) -> (fileNotFound + "\n" + incorrectNumber) |> (printfn "%s")
+
+[<EntryPoint>]
+let main argv = 
+    match argv with
+    | [|filename; number|] -> verify filename (parse number)
+    | _ -> printfn "Usage: LuckyWiner fileName numberOfLuckyWinners"
+    0
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LuckyWinner
+
+Simple F# application to pick random items from list provided in text file. More info coming SOON.
